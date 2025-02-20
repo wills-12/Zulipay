b847c78f85327423b4872da01328202043e44f9d
wills-12 <ekpenyonggodswill1@gmail.com>
2025-02-20 15:17:50 +0100

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: "567px",
+      md: "900px",
+      lg: "1200px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        darkBlue: "rgb(52, 114, 172)",
+        lightBlue: "rgb(105, 159, 209)",
+        lightGray: "rgb(179, 184, 190)",
+        veryLightGray: "rgb(255, 255, 255)",
+      },
+    },
+  },
+  plugins: [],
+};
